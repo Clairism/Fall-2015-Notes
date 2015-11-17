02448216e17acaa1400dda2facdd571ce1b52596
Claire Hu <hum085@newschool.edu>
2015-11-16 19:18:56 -0500

    no message

---
+++ Notes/Assets/CharacterBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBase : MonoBehaviour
+{
+
+	public float speed;
+	public float health;
+
+	public void Start ()
+	{
+		health = 100f;
+		speed = 10f;
+	}
+	
+	public void Update ()
+	{
+		float horz = Input.GetAxis ("Horizontal") * speed;
+		float vert = Input.GetAxis ("Vertical") * speed;
+	}
+
+	public void takeDamage ()
+	{
+
+	}
+}
---
+++ Notes/Assets/FasterCharacter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FasterCharacter : CharacterBase //the script's name writing under it.
+{
+
+	private Material mat;
+
+	public override void Start (Collision other)
+	{
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.mass = 20;
+		speed = 35f;
+		health = 10f;
+	}
+	
+	public void takeDamage (float damage)
+	{
+	}
+}
